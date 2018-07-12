29f49429438eca1f00d3e455c14b39596c9d1905
M@dmaker86 <cesarjarrie@gmail.com>
2018-07-12 16:47:52 -0300

    Feito crud usuario

--- vendor/hcodebr/php-classes/src/Model.php
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -24,8 +24,8 @@ class Model {
 		$method = substr($name, 0, 3);
 		$fieldName = substr($name, 3, strlen($name));
 
-		if (in_array($fieldName, $this->fields))
-		{
+		// if (in_array($fieldName, $this->fields))
+		// {
 			
 			switch ($method)
 			{
@@ -40,7 +40,7 @@ class Model {
 
 			}
 
-		}
+		// }
 
 	}
 
